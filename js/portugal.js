c6f6109871b737114363cd9a8a23a44c9dcb88e7
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-10-17 11:53:43 +0200

    réalisation de la page angleterre.html

---
+++ js/portugal.js
@@ -0,0 +1,15 @@
+const toggleP = (btnId , menuId)  => {
+    const btn = document.getEelemntById(btnId);
+    const menu = document.getElementById(menuId);
+
+    btn.addEventListener(
+        'click', (e)=>{e.stopPropagation();
+            menu.classList.toggle('hidden');
+        }
+    );
+    document.addEvenListener(
+        'click', ()=> menu.classList.add('hidden')
+    );
+    toggleP('continentBtnP','continentMenuP');
+    toggleP('personBtnP','personMenuP');
+};
